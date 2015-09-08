231b7696c763e68ef9febe354727ca2235f0df68
Jonathan Underwood <jonathan.underwood@gmail.com>
2015-09-08 01:12:16 +0100

    Remove cruft from setup.py

--- setup.py
+++ setup.py
@@ -34,42 +34,22 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
     name='outsider',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.1',
-
     description='Utilities for interfacing with Blackstar ID amplifiers',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/jonathanunderwood/outsider',
-
-    # Author details
     author='Jonathan Underwood',
     author_email='jonathan.underwood@gmail.com',
-
-    # Choose your license
     license='GPLv3+',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: End Users/Desktop',
         'Topic :: Multimedia :: Sound/Audio',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
@@ -78,14 +58,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-
-    # What does your project relate to?
     keywords='Blackstar Amplifiers',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['data', 'docs', 'tests*']),
-
     # Unfortunately we can't have 'PyQt5' here because unfortunately
     # it's not available on PyPi, so pip can't install it
     install_requires=['PyUSB'],
