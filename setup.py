5c5126bf14e3c70e83c03ac054a0113ef166dd13
Jonathan Underwood <jonathan.underwood@gmail.com>
2015-10-04 00:19:50 +0100

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
     name='outsider',
-    version='0.0.1',
+    version='0.0.2',
     description='Utilities for interfacing with Blackstar ID amplifiers',
     long_description=long_description,
     url='https://github.com/jonathanunderwood/outsider',
