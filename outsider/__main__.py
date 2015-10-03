459422a899615ba7b852c0505a71d0583942c45a
Jonathan Underwood <jonathan.underwood@gmail.com>
2015-10-03 20:53:21 +0100

    Tweak colour paletter and use fusion style

--- outsider/__main__.py
+++ outsider/__main__.py
@@ -18,6 +18,7 @@
 import outsider
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPalette, QColor
 import blackstarid
 import logging
 
@@ -32,13 +33,25 @@ def main(args=None):
     app = QtWidgets.QApplication(sys.argv)
     window = outsider.Ui()
 
-    # try:
-    #     amp = blackstarid.BlackstarIDAmp()
-    # except:
-    #     logger.error('No amplifier found')
-        #sys.exit(1)
-
-#    window.voice_changed_on_amp.emit(4)
+    # Tweak colors as per:
+    # https://gist.github.com/QuantumCD/6245215
+    app.setStyle("Fusion")
+    dark_palette = QPalette()
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, QColor(255, 255, 255))
+    dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, QColor(255, 255, 255))
+    dark_palette.setColor(QPalette.ToolTipText, QColor(255, 255, 255))
+    dark_palette.setColor(QPalette.Text, QColor(255, 255, 255))
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, QColor(255, 255, 255))
+    dark_palette.setColor(QPalette.BrightText, QColor(255, 0, 0))
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, QColor(0, 0, 0))
+    app.setPalette(dark_palette)
+    app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
 
     sys.exit(app.exec_())
 
