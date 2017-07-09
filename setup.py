e8e70c24cdc9b2b9b40ff1ea22382a887fc16fbc
Jonathan Underwood <jonathan.underwood@gmail.com>
2017-07-09 11:08:26 +0100

    Classify as beta software

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: End Users/Desktop',
         'Topic :: Multimedia :: Sound/Audio',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
